56280de275618de3a4cc161bd566ea3b9d208205
NestorGH <nestornica10@gmail.com>
2025-08-05 20:22:04 -0600

    added user dto and entity

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,17 @@
-export class CreateUserDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsString } from "class-validator";
+
+export class CreateUserDto {
+    @ApiProperty()
+    @IsString()
+    name: string
+
+    @ApiProperty()
+    password: string
+
+    @ApiProperty()
+    @IsString()
+    role: string
+
+    //appointment: CreateAppointmentDto[]
+}
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,20 @@
-export class User {}
+import { ApiProperty } from "@nestjs/swagger"
+
+export class UserEntity {
+    @ApiProperty()
+    name: string
+
+    @ApiProperty()
+    password: string
+
+    @ApiProperty()
+    role: string
+
+    //appointment: CreateAppointmentDto[]
+
+    @ApiProperty()
+    createdAt: Date;
+
+    @ApiProperty()
+    updatedAt: Date;
+}
