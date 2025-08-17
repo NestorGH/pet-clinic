7cf0b54f745710e07ae5d111df117c75d31f9704
NestorGH <nestornica10@gmail.com>
2025-08-17 16:21:46 -0600

    feat: added logic for vet-service

--- src/vet-service/vet-service.service.ts
+++ src/vet-service/vet-service.service.ts
@@ -1,29 +1,70 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateVetServiceDto } from './dto/create-vet-service.dto';
 import { UpdateVetServiceDto } from './dto/update-vet-service.dto';
 import { DatabaseService } from 'src/database/database.service';
+import { Prisma } from 'generated/prisma';
 
 @Injectable()
 export class VetServiceService {
-  constructor(private readonly databaseService: DatabaseService) {}
+  constructor(private readonly databaseService: DatabaseService) { }
 
   async create(createVetServiceDto: CreateVetServiceDto) {
-    return await 'This action adds a new vetService';
+    try {
+      return await this.databaseService.service.create({
+        data: {
+          name: createVetServiceDto.name,
+          description: createVetServiceDto.description,
+          price: createVetServiceDto.price
+        }
+      });
+    } catch (error) {
+      throw new BadRequestException('something bad happened')
+    }
   }
 
   async findAll() {
-    return await `This action returns all vetService`;
+    return await this.databaseService.service.findMany();
   }
 
   async findOne(id: number) {
-    return await `This action returns a #${id} vetService`;
+    const vetService = await this.databaseService.service.findUnique({ where: { id } });
+
+    if (!vetService) {
+      throw new NotFoundException('The record with the specified ID does not exist or has been deleted.');
+    }
+    return vetService;
   }
 
   async update(id: number, updateVetServiceDto: UpdateVetServiceDto) {
-    return await `This action updates a #${id} vetService`;
+    try {
+      return await this.databaseService.service.update({
+        where: { id },
+        data: {
+          name: updateVetServiceDto.name,
+          description: updateVetServiceDto.description,
+          price: updateVetServiceDto.price
+        }
+      });
+    } catch (error) {
+      if (
+        error instanceof Prisma.PrismaClientKnownRequestError &&
+        error.code === 'P2025'
+      ) {
+        throw new NotFoundException('The record with the specified ID does not exist or has been deleted.');
+      }
+      throw error;
+    }
   }
 
   async remove(id: number) {
-    return await `This action removes a #${id} vetService`;
+    const vetService = await this.databaseService.service.findUnique({where: {id}});
+    if (!vetService) {
+      throw new NotFoundException('The record with the specified ID does not exist or has been deleted.');
+    }
+
+    await this.databaseService.service.delete({
+      where: { id },
+    });
+    return vetService;
   }
 }
