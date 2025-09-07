c76af6cf7a3b626b8bca3d6e7514db4c67f0b0ef
NestorGH <nestornica10@gmail.com>
2025-09-07 11:51:55 -0600

    fix: removed unused import

--- src/appointment/dto/create-appoointment.dto.ts
+++ src/appointment/dto/create-appoointment.dto.ts
@@ -1,7 +1,6 @@
 import { ApiProperty } from "@nestjs/swagger"
 import { Type } from "class-transformer"
 import { IsArray, IsDate, IsDecimal, IsInt, IsNumber, IsString, ValidateNested } from "class-validator"
-import { CreateVetServiceDto } from "src/vet-service/dto/create-vet-service.dto"
 
 export class CreateAppointmentDto {
     @ApiProperty()
