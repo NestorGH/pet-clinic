3943089f450c96f77e2d3c1a303c64c33dc341cd
NestorGH <nestornica10@gmail.com>
2025-09-07 11:44:33 -0600

    fix: relative path

--- src/client/dto/create-client.dto.ts
+++ src/client/dto/create-client.dto.ts
@@ -1,7 +1,7 @@
 import { ApiProperty } from "@nestjs/swagger"
 import { Type } from "class-transformer"
 import { IsArray, IsEmail, IsString, ValidateNested } from "class-validator"
-import { CreatePetDto } from "src/pet/dto/create-pet.dto"
+import { CreatePetDto } from "../../pet/dto/create-pet.dto"
 
 export class CreateClientDto {
     @ApiProperty()
